baddf792cecdb090d1da672af67ff28653d7fd0b
Bharathy <rbharathy2999@gmail.com>
2021-10-30 10:37:31 -0400

    c

--- src/app/apis/apis.component.ts
+++ src/app/apis/apis.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AppserviceService } from 'src/app/appservice.service';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-apis',
   templateUrl: './apis.component.html',
@@ -7,12 +8,24 @@ import { AppserviceService } from 'src/app/appservice.service';
 })
 export class ApisComponent implements OnInit {
 
-  constructor(private apiserviceapp : AppserviceService) { }
+  constructor(private apiserviceapp : AppserviceService, private http: HttpClient ) { }
 
   ngOnInit(): void {
-    /*this.apiserviceapp.getdata().subscribe(res => {
+    this.apiserviceapp.getdata().subscribe(res => {
       console.log(res);
-    });*/
-  }
+    }),(console.error("Caught Error in Component")
+    );
+    }
 
+  // getdata(){
+  //   return new Promise( (resolve,reject) => {
+  //     var apiresponse = this.http.get("http://universities.hipolabs.com/search?country=United+States")
+  //     console.log(apiresponse);
+  //     if(apiresponse){
+  //       resolve(apiresponse)
+  //     }else{
+  //       reject("Failed")
+  //     }
+  //   }).then(x => {console.log(x)}).catch( y => console.log(y));
+  // }
 }
--- src/app/appservice.service.ts
+++ src/app/appservice.service.ts
@@ -9,10 +9,16 @@ export class AppserviceService {
   constructor(private http: HttpClient) { }
 
   getdata(){
-    return new Promise( (resolve,reject) => {
-      var apiresponse = this.http.get("http://universities.hipolabs.com/search?country=United+States")
-      console.log(apiresponse);
-    });
+    // return new Promise( (resolve,reject) => {
+    //   var apiresponse = this.http.get("http://universities.hipolabs.com/search?country=United+States")
+    //   if(apiresponse){
+    //     resolve("Success")
+    //   }else{
+    //     reject("Failed")
+    //   }
+    // }).then(x => {console.log(x)}).catch( y => console.log(y));
+    return this.http.post("http://universities.hipolabs.com/search?country=United+States",{title:"Bharathy"});
   }
+
 
 }
